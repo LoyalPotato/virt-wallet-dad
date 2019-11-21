5feba5c22e4cc2a26a8a996da64e1caf2960342d
Alex Pedro <alextpedro@gmail.com>
2019-11-21 22:30:56 +0000

    Add Vue and Vue Router boilerplate.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
-
+    .sass('resources/sass/app.scss', 'public/css')
+    .extract(['vue', 'axios']);
 mix.disableSuccessNotifications(); //NOTE: Para retirar notificaçoes de build successful com o node
