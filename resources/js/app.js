984a6ad31c72f321344ec3befddf0f1a99de874f
Alex Pedro <alextpedro@gmail.com>
2019-12-05 14:48:10 +0000

    Axios request for registration on RegisterComponent.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,24 @@
 import Vue from 'vue';
-import { store } from "./store/store";
-import VueRouter from "vue-router";
-import { routes } from "./routes";
+import { store } from './store/store';
+import VueRouter from 'vue-router';
+import { routes } from './routes';
 import MainComponent from './components/layouts/MainComponent.vue';
 
-Vue.component("main-comp", MainComponent);
+Vue.component('main-comp', MainComponent);
 
-window.Vue = require("vue");
+window.Vue = require('vue');
 Vue.use(VueRouter);
 
 const app = new Vue({
-    router: new VueRouter({mode: 'history', routes}),
-    store : store,
-    el: "#app",
-    components: {
-        MainComponent
-    }
+	router: new VueRouter({
+		mode: 'history',
+		routes
+	}),
+	store: store,
+	el: '#app',
+	components: {
+		MainComponent
+	}
 });
 
-
+document.window();
