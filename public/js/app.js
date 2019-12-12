d84a601961605bd9bb964f51224e59427e71e9f9
LoyalPotato <vidolitas@gmail.com>
2019-12-12 17:34:27 +0000

    Clean up

--- public/js/app.js
+++ public/js/app.js
@@ -2437,7 +2437,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "RegisterComponent",
   data: function data() {
